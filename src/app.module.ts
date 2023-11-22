58112157e5b90bfeefedc44a33f78a82f41fd756
TLN Nhăn <truonglapnhan1309@gmail.com>
2023-11-22 18:06:58 +0700

    add file .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './modules/products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Module({
-  imports: [ProductsModule, MongooseModule.forRoot(process.env.MONGOOSE_URL)],
+  imports: [ProductsModule, MongooseModule.forRoot(process.env.MONGODB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
